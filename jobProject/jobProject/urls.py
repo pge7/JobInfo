5ad2dbd7e9a20c7daa5de04e2e6144a96f34a79c
Yuge <gr500s@163.com>
2017-10-02 13:45:20 -0400

    Add files via upload

--- jobProject/jobProject/urls.py
+++ jobProject/jobProject/urls.py
@@ -1,7 +1,22 @@
-from django.conf.urls import include, url
-from django.contrib import admin
+from django.conf.urls import url
+from . import views
+
+app_name = 'MyProjects'
 
 urlpatterns = [
-    url(r'^admin/', admin.site.urls),
-    url(r'^job/', include('job.urls')),
+   #/MyProjects/detail
+    url(r'^detail/', views.detail, name = 'detail'),
+
+   #/MyProjects/index 
+    url(r'index/', views.index, name = 'index'),
+
+   #/MyProjects/index/jobquery 
+    url(r'jobquery/', views.jobquery, name = 'jobquery'),
+    
+    url(r'jobsearch_result/',views.search_job, name='jobsearch_result'),
+    
+    
+
 ]
+   
+
