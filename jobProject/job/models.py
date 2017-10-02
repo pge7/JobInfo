1cfff8e3557dd9f880ae59d95068e93f1cc1e0f2
Yuge <gr500s@163.com>
2017-10-02 13:53:29 -0400

    Update models.py

--- jobProject/job/models.py
+++ jobProject/job/models.py
@@ -1,11 +1,11 @@
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals
 from django.db import models
-
+from scrapy_djangoitem import DjangoItem
 # Create your models here.
 class Job(models.Model):
-    title = models.CharField(max_length=500)
-    location = models.CharField(max_length = 250)
-    date = models.IntegerField()
-    summary = models.CharField(max_length = 1000)
-    
+    title = models.CharField(max_length=500, default='no name')
+    location = models.CharField(max_length = 250, default='no name')
+    date = models.CharField(max_length =200, default='no name')
+    summary = models.CharField(max_length = 1000, default='no name')
+    joburl = models.CharField(max_length=3000, default='no name')
