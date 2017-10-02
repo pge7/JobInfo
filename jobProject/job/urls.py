febaa6894e6a90dbbac7e4c8fbf93eccd8c089e8
Yuge <gr500s@163.com>
2017-10-02 14:21:37 -0400

    Update urls.py

--- jobProject/job/urls.py
+++ jobProject/job/urls.py
@@ -4,5 +4,11 @@ from . import views
 urlpatterns = [
     #/job/
     url(r'^$', views.index, name='index'),
-    url(r'^jobquery/', views.jobquery, name='jobquery'),
+   
+    #/job/jobquery 
+    url(r'jobquery/', views.jobquery, name = 'jobquery'),
+    
+    #/job/jobsearch_result 
+    url(r'jobsearch_result/',views.search_job, name='jobsearch_result'),
+    
 ]
