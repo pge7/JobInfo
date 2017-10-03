0f85e385579ecc8b9937649c487cbfcce43706da
Yuge <gr500s@163.com>
2017-10-03 11:05:54 -0400

    add unique restain

--- jobProject/job/models.py
+++ jobProject/job/models.py
@@ -7,5 +7,9 @@ class Job(models.Model):
     title = models.CharField(max_length=500, default='no name')
     location = models.CharField(max_length = 250, default='no name')
     date = models.CharField(max_length =200, default='no name')
+    company = models.CharField(max_length=250, default = 'no name')
     summary = models.CharField(max_length = 1000, default='no name')
     joburl = models.CharField(max_length=3000, default='no name')
+    
+   class Meta: 
+       unique_together = ('title', 'company',)
